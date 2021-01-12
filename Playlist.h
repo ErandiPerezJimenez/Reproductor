40b7041999bd63884abaa71a831d7a4c7da119e4
Francisco Ortiz <davidfranom@gmail.com>
2021-01-12 17:03:15 -0600

    Archivo de declaración Versión 1

--- Playlist.h
+++ Playlist.h
@@ -11,6 +11,7 @@ typedef struct Track{
 	char performer[50];
 	char genre[50];
 	int year;
+	float duration;
 	
 	struct Track* next;
 	struct Track* prev;
@@ -23,5 +24,31 @@ typedef struct Playlist{
 	
 	size_t len;
 }Playlist;
+
+Playlist* Playlist_New();
+void Playlist_Delete(Playlist** this);
+
+bool Playlist_Insert_front( Playlist* this, int x );
+bool Playlist_Insert_back( Playlist* this, int x );
+bool Playlist_Insert( Playlist* this, int x );
+
+void Playlist_Remove_front( Playlist* this );
+void Playlist_Remove_back( Playlist* this );
+void Playlist_Remove( Playlist* this );
+
+int Playlist_Get( Playlist* this );
+
+void Playlist_Cursor_front( Playlist* this );
+void Playlist_Cursor_back( Playlist* this );
+void Playlist_Cursor_next( Playlist* this );
+void Playlist_Cursor_prev( Playlist* this );
+
+bool Playlist_IsEmpty( Playlist* this );
+size_t Playlist_Len( Playlist* this );
+
+void Playlist_MakeEmpty( Playlist* this );
+void Playlist_Traverse( Playlist* this, void (*fn)( int item  ) );
+
+#endif
 
 
