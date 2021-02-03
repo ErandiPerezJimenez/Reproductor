ac01d0782d2a7eb8f2b8fe91001488fc4d8d572d
dvgr1 <77249207+dvgr1@users.noreply.github.com>
2021-02-02 19:36:59 -0600

    Nuevas versiones de Playlist.h y .c
    Deberian ser las versiones definitivas, combinan el cuerpo de las funciones revisadas por David y el cambio en los argumentos y un campo de las estructura playlist modificadas por mi

--- Playlist.c
+++ Playlist.c
@@ -1,110 +1,112 @@
 #include "Playlist.h"
 
-static Node* newNode( Item x )
+
+static Node* newNode( Track* x )
 {
 	Node* n = (Node*)malloc(sizeof(Node));
 	if(n){
-		n->datos=x; //  Verificar si funciona
-		n->next=NULL;
-		n->prev=NULL;
+		n->datos = *x; //  Verificar si funciona
+		n->next = NULL;
+		n->prev = NULL;
 	}
 	return n;
 }
 
 Playlist* Playlist_New()
 {
-	Playlist* lista=(Playlist*)malloc(sizeof(Playlist));
+	Playlist* lista = (Playlist*)malloc(sizeof( Playlist ));
 	if(lista){
-		lista->cursor=NULL;
-		lista->first=NULL;
-		lista->last=NULL;
-		lista->len=0;
+		lista->cursor = NULL;
+		lista->first = NULL;
+		lista->last = NULL;
+		lista->len = 0;
 	} else{
 		printf("No se creo la lista");
 	}
 	return lista;
-}	
+}
 
 void Playlist_Delete( Playlist** this )
 {
-	for(size_t i=0;i<(*this)->len;++i){
-		Node* tmp=(*this)->first->next;
+	for(size_t i = 0; i < (*this)->len ;++i){
+		Node* tmp = (*this)->first->next;
 		free((*this)->first);
-		(*this)->first=tmp;
+		(*this)->first = tmp;
 	}
 	free(*this);
 	*this=NULL;
-}	
+}
 
-void Playlist_Insert( Playlist* this, Item x )
+void Playlist_Insert( Playlist* this, Track* x )
 {
 	Node* n= newNode(x);
-	if(Playlist_IsEmpty(this)){
-		this->first=this->last=this->cursor=n;
-		this->last->next=this->first;
-		this->first->prev=this->last;
+	if( Playlist_IsEmpty(this) ){
+		this->first = this->last = this->cursor = n;
+		this->last->next = this->first;
+		this->first->prev = this->last;
 	} else{
-		Node* tmp=this->cursor->next;
-		n->next=tmp;
-		tmp->prev=n;
-		if(this->cursor==this->last){
-			this->last=n;
+		Node* tmp = this->cursor->next;
+		n->next = tmp;
+		tmp->prev = n;
+
+		if(this->cursor == this->last){
+			this->last = n;
 		}
-		this->cursor->next=n;
-		n->prev=this->cursor;
-		this->cursor=n;
+		this->cursor->next = n;
+		n->prev = this->cursor;
+		this->cursor = n;
 	}
 	++this->len;
 }
 
-void Playlist_Insert_front( Playlist* this, Item x )
+void Playlist_Insert_front( Playlist* this, Track* x )
 {
 	Node* n= newNode(x);
 	if(Playlist_IsEmpty(this)){
-		this->first=this->last=this->cursor=n;
+		this->first = this->last = this->cursor = n;
 	} else{
-		n->next=this->first;
-		this->first->prev=n;
-		this->first=n;
+		n->next = this->first;
+		this->first->prev = n;
+		this->first = n;
 	}
-	this->last->next=this->first;
-	this->first->prev=this->last;
-	
+	this->last->next = this->first;
+	this->first->prev = this->last;
+
 	++this->len;
-	
+
 }
 
-void Playlist_Insert_back( Playlist* this, Item x )
+void Playlist_Insert_back( Playlist* this, Track* x )
 {
 	Node* n = newNode(x);
 	if(Playlist_IsEmpty(this)){
-		this->first=this->last=this->cursor=n;
+		this->first = this->last = this->cursor = n;
 	} else{
-		this->last->next=n;
-		n->prev=this->last;
-		this->last=n;
+		this->last->next = n;
+		n->prev = this->last;
+		this->last = n;
 	}
-	this->last->next=this->first;
-	this->first->prev=this->last;
-	
+	this->last->next = this->first;
+	this->first->prev = this->last;
+
 	++this->len;
-	
+
 }
 
 void Playlist_Remove( Playlist* this )
 {
-	assert(this->len);
-	
-	if(this->len==1){
+	assert( this->len > 0 );
+
+	if( this->len == 1 ){
 		free(this->first);
-		this->first=this->last=this->cursor=NULL;
+		this->first = this->last = this->cursor = NULL;
 		--this->len;
-	} else if(this->cursor==this->first){
+	} else if(this->cursor == this->first){
 		Playlist_Remove_front(this);
-		this->cursor=this->first;
-	} else if(this->cursor==this->last){
+		this->cursor = this->first;
+	} else if(this->cursor == this->last ){
 		Playlist_Remove_back(this);
-		this->cursor=this->last;
+		this->cursor = this->last;
 	} else{
 		Node* tmp1 = this->cursor->prev;
 		Node* tmp2 = this->cursor->next;
@@ -118,11 +120,11 @@ void Playlist_Remove( Playlist* this )
 
 void Playlist_Remove_front( Playlist* this )
 {
-	assert(this->len);
-	
-	if(this->len==1){
+	assert(this->len > 0);
+
+	if( this->len == 1 ){
 		free(this->first);
-		this->first=this->last=this->cursor=NULL;
+		this->first = this->last = this->cursor =  NULL;
 	} else{
 		Node* tmp=this->first->next;
 		free(this->first);
@@ -135,13 +137,13 @@ void Playlist_Remove_front( Playlist* this )
 
 void Playlist_Remove_back( Playlist* this )
 {
-	assert(this->len);
-	
-	if(this->len==1){
+	assert(this->len > 0);
+
+	if(this->len == 1){
 		free(this->last);
-		this->first=this->last=this->cursor=NULL;
+		this->first = this->last = this->cursor = NULL;
 	}else{
-		Node* tmp=this->last->prev;
+		Node* tmp = this->last->prev;
 		free(this->last);
 		this->last=tmp;
 		tmp->next=this->first;
@@ -151,23 +153,22 @@ void Playlist_Remove_back( Playlist* this )
 }
 
 
-Item Playlist_Get( Playlist* this ) // se tiene que modificar
+/*Track Playlist_Get( Playlist* this ) // se tiene que modificar
 {
-	assert(this->cursor);
 	return this->cursor->datos;
-}
+}*/
 
 
 void Playlist_Cursor_front( Playlist* this )
 {
-	assert(this->cursor);
-	this->cursor=this->first;
+    assert( this->cursor );
+	this->cursor = this->first;
 }
 
 void Playlist_Cursor_back( Playlist* this )
 {
-	assert(this->cursor);
-	this->cursor=this->last;
+    assert( this->cursor );
+	this->cursor = this->last;
 }
 
 void Playlist_Cursor_next( Playlist* this )
@@ -184,7 +185,7 @@ void Playlist_Cursor_prev( Playlist* this )
 
 bool Playlist_IsEmpty( Playlist* this )
 {
-	return this->len==0;
+	return this->len == 0;
 }
 
 size_t Playlist_Len( Playlist* this )
@@ -194,24 +195,24 @@ size_t Playlist_Len( Playlist* this )
 
 void Playlist_MakeEmpty( Playlist* this )
 {
-	for(size_t i=0; i<this->len;++i){
+	for(size_t i=0; i<this->len; ++i){
 		Playlist_Remove_front(this);
 	}
 }
 
 
-void Playlist_Traverse( Playlist* this, void (*fn)( Item item ) ) // se tiene que modificar
+void Playlist_Traverse( Playlist* this, void (*fn)( Track item ) ) // se tiene que modificar
 {
 	if( NULL == this ){ return; }
 
 	Node* t = this->first;
-   
-   
+
+
    do{
 		fn( t->datos );
 		t = t->next;
-		
+
    } while( t != this->first );
-   
+
 }
 
--- Playlist.h
+++ Playlist.h
@@ -1,55 +1,40 @@
-#ifndef  Playlist_INC
-#define  Playlist_INC
-
-#define MAX 50
+#ifndef  PLAYLIST_INC
+#define  PLAYLIST_INC
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <assert.h>
-
 #include "Track.h"
 
-/*
-typedef struct Track{
-	char title[MAX];
-	char performer[MAX];
-	char genre[MAX];
-	int year;
-	float duration;
-}Track;
-*/
-
-typedef Track* Item;
-
 typedef struct Node{
-	
-	Item datos;
+
+	Track datos;
 	struct Node* next;
 	struct Node* prev;
 } Node;
 
 typedef struct Playlist{
-	
+    char name[100]; //Nombre del playlist, en prueba
 	Node* first;
 	Node* last;
 	Node* cursor;
-	
 	size_t len;
-}Playlist;
+
+} Playlist;
 
 Playlist* Playlist_New();
-void Playlist_Delete(Playlist** this);
+void Playlist_Delete( Playlist** this );
 
-void Playlist_Insert_front( Playlist* this, Item x );
-void Playlist_Insert_back( Playlist* this, Item x );
-void Playlist_Insert( Playlist* this, Item x );
+void Playlist_Insert_front( Playlist* this, Track* x );
+void Playlist_Insert_back( Playlist* this, Track* x );
+void Playlist_Insert( Playlist* this, Track* x );
 
 void Playlist_Remove_front( Playlist* this );
 void Playlist_Remove_back( Playlist* this );
 void Playlist_Remove( Playlist* this );
 
-Item Playlist_Get( Playlist* this );
+Track Playlist_Get( Playlist* this );
 
 void Playlist_Cursor_front( Playlist* this );
 void Playlist_Cursor_back( Playlist* this );
@@ -60,6 +45,6 @@ bool Playlist_IsEmpty( Playlist* this );
 size_t Playlist_Len( Playlist* this );
 
 void Playlist_MakeEmpty( Playlist* this );
-void Playlist_Traverse( Playlist* this, void (*fn)( Item item  ) );
+void Playlist_Traverse( Playlist* this, void (*fn)( Track item  ) );
 
 #endif
